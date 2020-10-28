a952ed34cf34669246393b55fb7d6bce8073d161
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2020-10-28 12:07:37 +0100

    Remove participants and put event from frontend done With Janis

--- src/components/Event.js
+++ src/components/Event.js
@@ -24,6 +24,7 @@ import {
   faMinusCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import DateTimePicker from "./DateTimePicker";
+import { Context } from "../App";
 
 export default function Event() {
   const { id } = useParams();
@@ -32,36 +33,33 @@ export default function Event() {
 
   const toggle = () => setModal(!modal);
 
-  const [availableParticipants, setAllUsers] = useState([])
+  const [context, updateContext] = useContext(Context);
 
-  const [startTime, setStartTime] = useState({datum: "2020-11-27", tid: "09:00"});
-  const [endTime, setEndTime] = useState({datum: "2020-11-29", tid: "10:00"});
+  const [availableParticipants, setAvailableParticipants] = useState([]);
 
-  const [usersSelected, setUsersSelected] = useState(false)
+  const [startTime, setStartTime] = useState({
+    datum: "2020-11-27",
+    tid: "09:00",
+  });
+  const [endTime, setEndTime] = useState({ datum: "2020-11-29", tid: "10:00" });
 
   const [formData, setFormData] = useState({
     title: "",
     description: "",
     location: "",
     recurringEvent: 0,
-    participants: []
+    participants: [],
   });
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
-  async function fetchUsers(){
-    setAllUsers(await (await fetch('/api/users')).json())
+  async function fetchUsers() {
+    setAvailableParticipants(await (await fetch("/api/users")).json())
   }
 
-  let {
-    title,
-    description,
-    location,
-    recurringEvent,
-    participants,
-  } = formData;
+  let { title, description, location, recurringEvent, participants } = formData;
 
   useEffect(() => {
     // If id is "new" then do nothing - we don't need to fetch data
@@ -79,9 +77,32 @@ export default function Event() {
     }
     // Since you should not make useEffect functions async, we need to create another function that is async
     // in order to use await with our fetch
-    (async () =>
-      setFormData(await (await fetch("/api/events/" + id)).json()))();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    (async () => {
+      let result = await (await fetch("/api/events/" + id)).json()
+      // result.invited = result.invited.map(user => ({value : user.userId, label: user.email}))
+      let p = [...result.invited, ...result.participants].filter(user => user)
+      .map(user => ({value : user.userId, label: user.email}))
+      // console.log("invited i useEffect", result.invited);
+      // console.log("p i useEffect", p);
+      result.id = result.eventId
+      delete result.eventId
+      delete result.userName
+      delete result.email
+      delete result.invited
+      console.log("result i useEffect", result);
+      
+      setFormData({
+        ...result,
+        participants: p
+      })
+      
+      // console.log("result.eventId efter setFormData", eventId);
+      console.log("formData i useEffect", formData);
+      // console.log("result i useEffect", result);
+    }
+    )
+    ();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const handleInputChange = (e) => {
@@ -89,67 +110,79 @@ export default function Event() {
       ...formData,
       [e.currentTarget.name]: e.currentTarget.value,
     });
-  }
+  };
 
   // När Event.js blir en modal, ersätt nedanstående if med att stänga modalen
   if (formData.done) {
     return <Redirect to="/myCalendar" />;
   }
-  
+
   if (title === undefined) {
     return null;
   }
-  
+
   function cancel() {
     setFormData({ done: true });
   }
 
   function modalSuccess() {
     let selectAvailable = document.querySelector('[name="selectAvailable"]');
-    let opts = []
+    let opts = [];
     for (var opt of selectAvailable.options) {
       if (opt.selected) {
-        opts.push({label: opt.label, value: opt.value});
+        opts.push({ label: opt.label, value: opt.value });
       }
     }
 
-    participants.push(...opts)
+    participants.push(...opts);
 
     toggle();
   }
 
   async function save(e) {
-    formData.endDateTime = new Date(`${endTime.datum}, ${endTime.tid}`).toLocaleString();
-    formData.startDateTime = new Date(`${startTime.datum}, ${startTime.tid}`).toLocaleString();
-    formData.participants = participants.map(user => ({ userId: user.value }))
+    console.log("save, formData: ", formData);
+    console.log("save, participants", formData.participants);
+    formData.endDateTime = new Date(
+      `${endTime.datum}, ${endTime.tid}`
+    ).toLocaleString();
+    formData.startDateTime = new Date(
+      `${startTime.datum}, ${startTime.tid}`
+    ).toLocaleString();
+    formData.participants = participants.map((user) => ({
+      userId: user.value,
+    }));
 
-    e.preventDefault()
-    // Send the data to the REST api
+    e.preventDefault();
+    //Send the data to the REST api
     await fetch("/api/events/" + (id === "new" ? "" : id), {
-      method: (id === "new" ? "POST" : "PUT"),
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(formData)
-    })
- 
-    setFormData({done: true})
+      method: id === "new" ? "POST" : "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    setFormData({ done: true });
   }
 
   function filterAvailableParticipants() {
-    return availableParticipants.filter(f => !participants.find(i => f.id == i.value))
+    return availableParticipants.filter(
+      (f) => (context.user && context.user.id != f.id) && !participants.find((i) => i && (f.id == i.value))
+    );
   }
 
   function removeParticipantHandler() {
     let selectAvailable = document.querySelector('[name="selectParticipants"]');
-    let opts = []
+    let opts = [];
     for (var opt of selectAvailable.options) {
       if (opt.selected) {
-        opts.push({label: opt.label, value: opt.value});
+        opts.push({ label: opt.label, value: opt.value });
       }
     }
 
-    participants.pop(opts)
-    // participants = participants.filter(u1 => !opts.find(u2 => u1.id == u2.value))
-    console.log(opts);
+    let p = participants.filter(u1 => !opts.find(u2 => u1.value == u2.value))
+    setFormData({
+      ...formData,
+      participants: p
+    })
   }
 
   return (
@@ -189,25 +222,25 @@ export default function Event() {
           </ModalFooter>
         </Modal>
       </div>
-      
-      <Form className="mb-5">
-      <Row form>
-        <Col>
-          <h5 className="d-inline-block p-2">
-            {id === "new" ? "Ny händelse" : "pilledutt"}
-          </h5>
 
-          {id !== "new" ? (
-            <FontAwesomeIcon
-              className=" float-right my-2"
-              size="2x"
-              icon={faTrash}
-            />
-          ) : (
-            ""
-          )}
-        </Col>
-      </Row>
+      <Form className="mb-5">
+        <Row form>
+          <Col>
+            <h5 className="d-inline-block p-2">
+              {id === "new" ? "Ny händelse" : "pilledutt"}
+            </h5>
+
+            {id !== "new" ? (
+              <FontAwesomeIcon
+                className=" float-right my-2"
+                size="2x"
+                icon={faTrash}
+              />
+            ) : (
+              ""
+            )}
+          </Col>
+        </Row>
         <Row form>
           <Col xs="12" md="6">
             <FormGroup>
@@ -282,12 +315,20 @@ export default function Event() {
                 className="float-right text-success"
                 onClick={toggle}
               />
-              <FontAwesomeIcon
-                size="lg"
-                icon={faMinusCircle}
-                className="float-right text-danger mr-2"
-                onClick={removeParticipantHandler}
-              />
+              {participants.length > 0 ? (
+                <FontAwesomeIcon
+                  size="lg"
+                  icon={faMinusCircle}
+                  className="float-right text-danger mr-2"
+                  onClick={removeParticipantHandler}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  size="lg"
+                  icon={faMinusCircle}
+                  className="float-right mr-2"
+                />
+              )}
               {/* lägg in deltagare i value */}
               <Input
                 type="select"
@@ -297,6 +338,7 @@ export default function Event() {
               >
                 {participants.map((e, key) => {
                   return (
+                    e &&
                     <option key={key} value={e.value}>
                       {e.label}
                     </option>
